df706e6fc11df605515223622460fffbd1437dec
Hoang  Bui <hhb5051@psu.edu>
2020-08-28 05:13:45 +0000

    delete 'in' in print

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 cel = float(input("Enter temperature in celsius: "))
 fah = cel * 9/5 + 32
-print(f"{cel}{chr(176)} in Celsius is equivalent to {fah}{chr(176)} in Fahrenheit.")
+print(f"{cel}{chr(176)} in Celsius is equivalent to {fah}{chr(176)} Fahrenheit.")
